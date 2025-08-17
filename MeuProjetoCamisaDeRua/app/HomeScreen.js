586c34a8152594b54f0557ecfa688a9ab3fcba07
Victor Andrade Domingos da Silva <vito.andrade01@gmail.com>
2025-08-17 12:12:13 -0300

    Pacotes instalados para o incio das telas e do projeto.

---
+++ MeuProjetoCamisaDeRua/app/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Bem-vindo à Store Camisa de Rua!</Text>
+    </View>
+  );
+}
